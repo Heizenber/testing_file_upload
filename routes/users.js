7e9be2ff39d4c17b42058d10f5f0b225ef06270c
Heizenberg <popov.roman91@gmail.com>
2024-01-08 09:24:26 +0200

    added images upload functionality

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const multer = require('multer');
+
+const storageConfig = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'images')
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + '-' + file.originalname)
+  }
+})
 
 const router = express.Router();
+const upload = multer({storage: storageConfig})
 
-router.get('/', function(req, res) {
+router.get('/', (req, res) => {
   res.render('profiles');
 });
 
-router.get('/new-user', function(req, res) {
+router.get('/new-user',  (req, res) => {
   res.render('new-user');
 });
+
+router.post('/profiles', upload.single('image'), (req, res) => {
+  const uploadedImageFile = req.file;
+  const userData = req.body;
+
+  console.log(uploadedImageFile);
+  console.log(userData);
+
+  res.redirect('/');
+})
 
 module.exports = router;
